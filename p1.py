a89580e38a4f66e097fd97e925f09dbdfc5cf008
Ira <iramonko97@gmail.com>
2017-05-30 00:25:55 +0300

    Interpolates and reclassify raster

--- p1.py
+++ p1.py
@@ -3,3 +3,7 @@ from arcpy.sa import *
 arcpy.env.workspace=r'C:\Users\Ira\Dropbox\Study\ProgPy\P1_3'
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.overwriteOutput = True
+a=Idw(r'C:\Users\Ira\Dropbox\Study\ProgPy\Lesson1\Precip2008Readings.shp','RASTERVALU')
+b=Reclassify(a,'Value',RemapRange([[27715,55521,1],[55521,83326,2],[83326,111133,3]]))
+arcpy.RasterToPolygon_conversion(b,r'C:\Users\Ira\Dropbox\Study\ProgPy\P1_3\poly.shp',"NO_SIMPLIFY",'VALUE')
+arcpy.Clip_analysis(r'C:\Users\Ira\Dropbox\Study\ProgPy\P1_3\poly.shp',r'C:\Users\Ira\Dropbox\Study\ProgPy\Lesson1\Nebraska.shp',r'C:\Users\Ira\Dropbox\Study\ProgPy\P1_3\polyclip.shp')
