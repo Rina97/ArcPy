fe5d778126b3a7cd797c11cd55ec73c02e1f31ce
Ira <iramonko97@gmail.com>
2017-05-30 01:20:29 +0300

    Make script

--- p2.py
+++ p2.py
@@ -1,3 +1,13 @@
 import arcpy
 import os
 arcpy.env.workspace = arcpy.GetParameterAsText(0)
+target=arcpy.GetParameterAsText(1)
+fcList = arcpy.ListFeatureClasses()
+for fc in fcList:
+    desc = arcpy.Describe(fc)
+    if desc.spatialReference.name!=arcpy.Describe(target).spatialReference.name:
+        outfc=desc.name[:-4]+'_projected.shp'
+        arcpy.Project_management(fc,os.path.join(arcpy.env.workspace,outfc),arcpy.Describe(target).spatialReference)
+    #arcpy.AddMessage(desc.spatialReference.name)
+
+print "Script completed"
